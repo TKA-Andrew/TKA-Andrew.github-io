4d836a6da44a2929c28877e09b8f82bc22439c79
TKA-Andrew <kui.an.andrew.tan@gmail.com>
2021-08-08 12:12:16 +0800

    added header

--- src/app/main-contents/main-contents.module.ts
+++ src/app/main-contents/main-contents.module.ts
@@ -3,15 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { MainContentsRoutingModule } from './main-contents-routing.module';
 import { MainContentsComponent } from './main-contents.component';
-
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import  {MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    MainContentsComponent
+    MainContentsComponent,
+    TopbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    MainContentsRoutingModule
+    MainContentsRoutingModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule
   ]
 })
 export class MainContentsModule { }
---
+++ src/app/main-contents/topbar/topbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-content-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss']
+})
+export class TopbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
