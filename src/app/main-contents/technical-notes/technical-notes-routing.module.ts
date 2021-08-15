5dd915863d9823ecbc82094debe160b89d941d44
TKA-Andrew <kui.an.andrew.tan@gmail.com>
2021-08-15 21:18:02 +0800

    modified technical notes layout

--- src/app/main-contents/technical-notes/technical-notes-routing.module.ts
+++ src/app/main-contents/technical-notes/technical-notes-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GeneralComponent } from './coding/general/general.component';
 import { TechnicalNotesComponent } from './technical-notes.component';
 
 const routes: Routes = [{
   path:'',
   component:TechnicalNotesComponent,
+  children: [
+    {
+      path: 'general',
+      component: GeneralComponent
+    }
+  ]
 }];
 
 @NgModule({
--- src/app/main-contents/technical-notes/technical-notes.module.ts
+++ src/app/main-contents/technical-notes/technical-notes.module.ts
@@ -10,9 +10,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { GeneralComponent } from './coding/general/general.component';
 
 @NgModule({
-  declarations: [TechnicalNotesComponent],
+  declarations: [TechnicalNotesComponent, GeneralComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
