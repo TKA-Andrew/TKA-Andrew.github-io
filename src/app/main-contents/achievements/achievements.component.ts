64b6d606437730fd255fbf1141c3eb9b3bcdb4f0
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-01-15 16:48:16 +0800

    added background image laoded check

--- src/app/main-contents/achievements/achievements.component.ts
+++ src/app/main-contents/achievements/achievements.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import SwiperCore, { Navigation } from "swiper";
+import { ShareFacadeService } from 'src/app/services/shareFacade.service';
 
 // install Swiper modules
 SwiperCore.use([Navigation]);
@@ -22,9 +23,16 @@ export class AchievementsComponent implements OnInit {
   dotAnimation: boolean = true;
   side = 'left';
 
-  constructor() { }
+  constructor(
+    private shareFacadeService: ShareFacadeService
+  ) { }
 
   ngOnInit(): void {
+    this.shareFacadeService.startSpinner();
+  }
+
+  loaded(): void {
+    this.shareFacadeService.stopSpinner();
   }
 
 
