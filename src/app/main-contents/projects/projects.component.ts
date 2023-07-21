90d1225d689805649be4c4760d610224342fad16
tka-andrew <kui.an.andrew.tan@gmail.com>
2023-07-22 02:24:09 +0800

    Refactor code

--- src/app/main-contents/projects/projects.component.ts
+++ src/app/main-contents/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, AfterContentInit, Renderer2, ChangeDetectorRef } from '@angular/core';
 
 export interface ISlideIndexHash {
   [projectName: string] : number
@@ -10,71 +10,57 @@ export interface ISlideIndexHash {
   styleUrls: ['./projects.component.scss']
 })
 
-export class ProjectsComponent implements OnInit {
-  public slideIndex: number = 1
-  public slideIndexHash: ISlideIndexHash = {};  
+export class ProjectsComponent implements AfterContentInit {
+  public slideIndexHash: ISlideIndexHash = {};
+  public projectImagesCount = {
+    'path-finding-visualizer': Array(5),
+    'path-planning-visualizer': Array(6),
+    'image-based-sudoku-solver': Array(1),
+  }
 
   constructor(
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private changeDetectorRef: ChangeDetectorRef
   ) { }
 
-  ngOnInit(): void {
-    this.slideIndexHash['path-finding-visualizer'] = 1;
-    this.slideIndexHash['path-planning-visualizer'] = 1;
-    this.slideIndexHash['image-based-sudoku-solver'] = 1;
-    this.showSlides('path-finding-visualizer', this.slideIndex);
-    this.showSlides('path-planning-visualizer', this.slideIndex);
-    this.showSlides('image-based-sudoku-solver', this.slideIndex);
+  ngAfterContentInit(): void {
+    this.slideIndexHash['path-finding-visualizer'] = 0;
+    this.slideIndexHash['path-planning-visualizer'] = 0;
+    this.slideIndexHash['image-based-sudoku-solver'] = 0;
+
+    // Trigger change detection manually to ensure the template is updated before showSlide()
+    this.changeDetectorRef.detectChanges();
+
+    this.showSlide('path-finding-visualizer', this.slideIndexHash['path-finding-visualizer']);
+    this.showSlide('path-planning-visualizer', this.slideIndexHash['path-planning-visualizer']);
+    this.showSlide('image-based-sudoku-solver', this.slideIndexHash['image-based-sudoku-solver']);
   }
 
-  plusSlides(project: string, n: number): void {
-    this.showSlides(project, this.slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  currentSlide(project: string, n: number): void {
-    this.showSlides(project, this.slideIndex = n);
-  }
-
-  showSlides(project: string, n: number): void {
-  
-    let slide_class = "";
-    let dot_class = "";
-
-    switch(project)
-    {
-      case 'path-finding-visualizer':
-        slide_class = 'slide--path-finding-visualizer';
-        dot_class = 'dot--path-finding-visualizer';
-        break;
-        
-      case 'path-planning-visualizer':
-        slide_class = 'slide--path-planning-visualizer';
-        dot_class = 'dot--path-planning-visualizer';
-        break;
-
-      case 'image-based-sudoku-solver':
-        slide_class = 'slide--image-based-sudoku-solver';
-        dot_class = 'dot--image-based-sudoku-solver';
-        break;
-
-      default:
-        break;
+  updateSlideIndex(project: string, x: number) {
+    if (this.slideIndexHash[project] !== undefined) {
+      this.slideIndexHash[project] += x;
+      this.showSlide(project, this.slideIndexHash[project]);
     }
+  }
 
-    let slides = document.getElementsByClassName(slide_class);
-    let dots = document.getElementsByClassName(dot_class);
-    if (n > slides.length) {this.slideIndex = 1}
-    if (n < 1) {this.slideIndex = slides.length}
-    // REFENCE: https://stackoverflow.com/questions/48265353/how-to-style-a-child-html-element-in-typescript-angular
+  showSlide(project: string, slideIndex: number): void {
+    let slides = document.getElementsByClassName("slide--" + project) as HTMLCollectionOf<HTMLElement>;
+    let dots = document.getElementsByClassName("dot--" + project) as HTMLCollectionOf<HTMLElement>;
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+    else if (slideIndex < 0) {
+      slideIndex = slides.length - 1
+    }
     for (let i = 0; i < slides.length; i++) {
       this.renderer.setStyle(slides[i], 'display', 'none');
     }
     for (let i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].classList.remove('active');
     }
-    this.renderer.setStyle(slides[this.slideIndex-1], 'display', 'block');
-    dots[this.slideIndex-1].className += " active";
+    dots[slideIndex].classList.add('active')
+    this.renderer.setStyle(slides[slideIndex], 'display', 'block');
+    this.slideIndexHash[project] = slideIndex
   }
 
 }
